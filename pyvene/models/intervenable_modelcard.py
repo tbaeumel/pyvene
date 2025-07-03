01dae5ac07b55c628799f2d289a61a8bace21c81
tbaeumel <tanja.baeumel@gmx.de>
2025-07-03 11:28:16 +0200

    Added Olmo-2 Model to intervenable_modelcard.py

--- pyvene/models/intervenable_modelcard.py
+++ pyvene/models/intervenable_modelcard.py
@@ -14,6 +14,7 @@ from .backpack_gpt2.modelings_intervenable_backpack_gpt2 import *
 from .llava.modelings_intervenable_llava import *
 from .qwen2.modelings_intervenable_qwen2 import * 
 from .olmo.modelings_intervenable_olmo import *
+from .olmo2.modelings_intervenable_olmo2 import *
 from .esm.modelings_intervenable_esm import *
 
 #########################################################################
@@ -64,7 +65,9 @@ type_to_module_mapping = {
     hf_models.gemma2.modeling_gemma2.Gemma2Model: gemma2_type_to_module_mapping,
     hf_models.gemma2.modeling_gemma2.Gemma2ForCausalLM: gemma2_lm_type_to_module_mapping,
     hf_models.olmo.modeling_olmo.OlmoModel: olmo_type_to_module_mapping,
-    hf_models.olmo.modeling_olmo.OlmoForCausalLM: olmo_lm_type_to_module_mapping,  
+    hf_models.olmo.modeling_olmo.OlmoForCausalLM: olmo_lm_type_to_module_mapping,
+    hf_models.olmo2.modeling_olmo2.Olmo2Model: olmo2_type_to_module_mapping,
+    hf_models.olmo2.modeling_olmo2.Olmo2ForCausalLM: olmo2_lm_type_to_module_mapping,
     hf_models.esm.modeling_esm.EsmModel: esm_type_to_module_mapping,
     hf_models.esm.modeling_esm.EsmForMaskedLM: esm_mlm_type_to_module_mapping,
     hf_models.blip.modeling_blip.BlipForQuestionAnswering: blip_type_to_module_mapping,
@@ -103,7 +106,9 @@ type_to_dimension_mapping = {
     hf_models.gemma2.modeling_gemma2.Gemma2Model: gemma2_type_to_dimension_mapping,
     hf_models.gemma2.modeling_gemma2.Gemma2ForCausalLM: gemma2_lm_type_to_dimension_mapping,
     hf_models.olmo.modeling_olmo.OlmoModel: olmo_type_to_dimension_mapping,
-    hf_models.olmo.modeling_olmo.OlmoForCausalLM: olmo_lm_type_to_dimension_mapping, 
+    hf_models.olmo.modeling_olmo.OlmoForCausalLM: olmo_lm_type_to_dimension_mapping,
+    hf_models.olmo2.modeling_olmo2.Olmo2Model: olmo2_type_to_dimension_mapping,
+    hf_models.olmo2.modeling_olmo2.Olmo2ForCausalLM: olmo2_lm_type_to_dimension_mapping,
     hf_models.esm.modeling_esm.EsmModel: esm_type_to_dimension_mapping,
     hf_models.esm.modeling_esm.EsmForMaskedLM: esm_mlm_type_to_dimension_mapping,
     hf_models.blip.modeling_blip.BlipForQuestionAnswering: blip_type_to_dimension_mapping,
